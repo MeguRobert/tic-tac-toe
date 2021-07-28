f6aec8b610365a4fd3a3f86e642414384886aa34
MeguRobert <megurobi14@gmail.com>
2021-07-28 20:55:06 +0300

    first deploy

--- src/components/Square.js
+++ src/components/Square.js
@@ -5,7 +5,8 @@ export default function Square(props) {
     const styles = {
         color: "#47d7df",
         fontSize: '8rem',
-        backgroundColor: "#4793df"
+        backgroundColor: "#4793df",
+        border: "1px solid"
     }
     
     return (
